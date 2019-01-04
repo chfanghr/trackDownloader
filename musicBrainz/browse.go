d27b64b496834b9721c0218e3b11d4161b2bcec8
fanghr <chfanghr@gmail.com>
2019-01-04 23:15:22 +0800

    Update

--- musicBrainz/browse.go
+++ musicBrainz/browse.go
@@ -88,7 +88,7 @@ var possibleBrowseEntity = map[string]struct {
 			res = append(res, possibleBrowseIncForAllEntity...)
 			res = append(res, possibleBrowseIncForAllEntityExceptPlaceAndReleaseAndSeries...)
 			res = append(res, possibleBrowseIncForAllEntityExceptRelease...)
-			res = append(res, possibleBrowseIncForAllEntityExceptRecordingAndReleaseAndReleaseGroup...)
+			res = append(res, "artist-credit", "isrcs")
 			return
 		}()...),
 	},
@@ -125,6 +125,7 @@ var possibleBrowseEntity = map[string]struct {
 		strings.Split("area,artist,collection,label,track,track_artist,recording,release-group", ","),
 		append([]string{}, func() (res []string) {
 			res = append(res, possibleBrowseIncForAllEntity...)
+			res = append(res, "artist-credit", "labels", "recordings", "release-groups", "media", "discids", "isrcs")
 			return
 		}()...),
 	},
@@ -134,6 +135,7 @@ var possibleBrowseEntity = map[string]struct {
 			res = append(res, possibleBrowseIncForAllEntity...)
 			res = append(res, possibleBrowseIncForAllEntityExceptPlaceAndReleaseAndSeries...)
 			res = append(res, possibleBrowseIncForAllEntityExceptRelease...)
+			res = append(res, "artist-credit")
 			return
 		}()...),
 	},
@@ -147,46 +149,51 @@ var possibleBrowseEntity = map[string]struct {
 	},
 }
 
-func BuildBrowseRequest(entity string, param struct {
-	linkedEntityAndValue []struct {
-		linkedEntity string
-		value        string
-	}
-	offset string
-	limit  string
-	inc    []string
+type BuildBrowseRequestParamLinkedEntityAndValue struct {
+	LinkedEntity string
+	Value        string
+}
 
-	filterByType   string
-	filterByStatus string
-}) (req *http.Request, err error) {
+type BuildBrowseRequestParam struct {
+	LinkedEntityAndValue []BuildBrowseRequestParamLinkedEntityAndValue
+	Offset               string
+	Limit                string
+	Inc                  []string
+
+	FilterByType   []string
+	FilterByStatus []string
+}
+
+func BuildBrowseRequest(entity string, param BuildBrowseRequestParam) (req *http.Request, err error) {
 	if _, ok := possibleBrowseEntity[entity]; !ok {
 		err = fmt.Errorf("unsuportted entity %s", entity)
 		return
 	}
 	var url string = APIRoot + "/" + entity
 	var query string
-	if len(param.linkedEntityAndValue) > 0 {
+
+	if len(param.LinkedEntityAndValue) > 0 {
 		var tmp string
-		for _, v := range param.linkedEntityAndValue {
-			if v.linkedEntity == "" {
+		for _, v := range param.LinkedEntityAndValue {
+			if v.LinkedEntity == "" {
 				continue
 			}
-			if !isStringInStrings(v.linkedEntity, possibleBrowseEntity[entity].linkedEntity...) {
-				err = fmt.Errorf("unsupported linked entity %s for entity %s", v.linkedEntity, entity)
+			if !isStringInStrings(v.LinkedEntity, possibleBrowseEntity[entity].linkedEntity...) {
+				err = fmt.Errorf("unsupported linked entity %s for entity %s", v.LinkedEntity, entity)
 				return
 			}
 			if tmp != "" {
 				tmp += "&"
 			}
-			tmp = tmp + v.linkedEntity + "=" + v.value
+			tmp = tmp + v.LinkedEntity + "=" + v.Value
 		}
 		if tmp != "" {
 			query += tmp
 		}
 	}
-	if len(param.inc) > 0 {
+	if len(param.Inc) > 0 {
 		var incs string
-		for _, v := range param.inc {
+		for _, v := range param.Inc {
 			if v == "" {
 				continue
 			}
@@ -206,20 +213,68 @@ func BuildBrowseRequest(entity string, param struct {
 			query += incs
 		}
 	}
-	if param.offset != "" {
-		if query != "" {
-			query += "&"
+	if isStringInStrings(entity, "release", "release-group") {
+		if len(param.FilterByType) > 0 {
+			var filterTypes string
+			for _, v := range param.FilterByType {
+				if v == "" {
+					continue
+				}
+				if !isStringInStrings(v, possibleReleaseType...) {
+					err = fmt.Errorf("unsupported filter type %s", v)
+					return
+				}
+				if filterTypes != "" {
+					filterTypes += "|"
+				}
+				filterTypes += v
+			}
+			if filterTypes != "" {
+				if query != "" {
+					query += "&"
+				}
+				query = query + "type=" + filterTypes
+			}
 		}
-		query = query + "offset=" + param.offset
 	}
-	if param.limit != "" {
+	if isStringInStrings(entity, "release") {
+		if len(param.FilterByStatus) > 0 {
+			var filterStatus string
+			for _, v := range param.FilterByStatus {
+				if v == "" {
+					continue
+				}
+				if !isStringInStrings(v, possibleReleaseStatus...) {
+					err = fmt.Errorf("unsupported filter status %s", v)
+					return
+				}
+				if filterStatus != "" {
+					filterStatus += "|"
+				}
+				filterStatus += v
+			}
+			if filterStatus != "" {
+				if query != "" {
+					query += "&"
+				}
+				query = query + "status=" + filterStatus
+			}
+		}
+	}
+	if param.Offset != "" {
 		if query != "" {
 			query += "&"
 		}
-		query = query + "limit=" + param.limit
+		query = query + "offset=" + param.Offset
+	}
+	if param.Limit != "" {
+		if query != "" {
+			query += "&"
+		}
+		query = query + "limit=" + param.Limit
 	}
 	if query != "" {
 		url = url + "?" + query
 	}
-	return
+	return http.NewRequest("GET", url, nil)
 }
