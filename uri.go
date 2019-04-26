f31670f87b4ad9a09fcab8d00456bd3ce476a48d
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-04-26 15:30:00 +0800

    Create uri.go

---
+++ uri.go
@@ -0,0 +1,15 @@
+package main
+
+type URIType int
+
+//TODO
+type URI struct{}
+
+//TODO
+func URIFromString(string)URI{panic(nil)}
+
+//TODO
+func (u URI)Type()URIType{panic(nil)}
+
+//TODO
+func (u URI)Base62ID()string{panic(nil)}
