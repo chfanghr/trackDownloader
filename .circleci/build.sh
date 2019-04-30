a4f7d46fa1f589f8084ae5d28d052f49cc67d581
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-04-30 13:17:20 +0600

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -5,9 +5,9 @@ sudo chmod +666 /build
 GO111MODULE=on go get -d ./...
 
 go_build(){
-local sf=""
-for "windows" in $1 ;do sf= ".exe ";done
-GO111MODULE=on GOOS=$1 GOARCH=$2 go build -o /build/$1_$2$sf $3
+#local sf=""
+#for "windows" in $1 ;do sf= ".exe ";done
+GO111MODULE=on GOOS=$1 GOARCH=$2 go build -o /build/$1_$2 $3
 }
 
 go_build "linux" "amd64" $1
@@ -19,7 +19,7 @@ go_build "windows" "386" $1
 go_build "darwin" "amd64" $1
 go_build "darwin" "386" $1
 
-#for i in /build/windows*;do mv $i $i.exe; done
+for i in /build/windows*;do mv $i $i.exe; done
 
 #tar -cf /tmp/build.tar /build
 #mv /tmp/build.tar /build
